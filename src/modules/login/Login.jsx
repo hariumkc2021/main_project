53a8724294edc61269f588651697947a126d39cd
sandeep <Sandeep_Perkari@neimanmarcus.com>
2021-12-11 15:39:23 -0600

    COMKIT

--- src/modules/login/Login.jsx
+++ src/modules/login/Login.jsx
@@ -5,13 +5,17 @@ import { AppContext } from '../../contexts/AppContext';
 import "./Login.css";
 import Alert from '@material-ui/lab/Alert';
 import axios from 'axios';
+import Box from '@material-ui/core/Box'
+
 const Login = (props) => {
     const history = useHistory();
     const { setUser } = useContext(AppContext);
     const [enableToat, setToast] = useState(false);
     const [toastTxt, setToastTxt] = useState("");
+    const [loginComp,setLoginComp]=useState(true);
 
     const onLogin = (e) => {
+      if(loginComp){
       if (e.target.email.value == "" || e.target.password.value == "") {
         setToast(true);
         setToastTxt("Feilds cannot be empty");
@@ -36,20 +40,63 @@ const Login = (props) => {
             setToast(true);
             setToastTxt("Invalid input");
           });
+      }}else{
+        setLoginComp(true);
       }
       e.preventDefault();
     };
+
+    const signUp =(e)=>{
+      if(!loginComp){
+        if (e.target.email.value == "" || e.target.password.value == ""||e.target.name.value=="" ) {
+          setToast(true);
+          setToastTxt("Feilds cannot be empty");
+        }else{
+          axios
+          .post("https://umkc-project.herokuapp.com/signUp", {
+            "name": e.target.name.value,
+            "password": e.target.password.value,
+            "email":e.target.email.value,
+          })
+          .then((response) => {
+            setToastTxt("User created");
+            setLoginComp(true);
+          }).catch((e) => {
+            setToast(true);
+            setToastTxt("Invalid input");
+          });
+        }
+
+      }else{
+        setLoginComp(false);
+      }
+      e.preventDefault();
+    }
+
     return (
         <>
             {enableToat ? <Alert variant="filled" severity="error">
                 {toastTxt}</Alert> : null}
-            < div className="login-container">
+            {loginComp?< div className="login-container">
                 <form className="login-box" onSubmit={e => onLogin(e)}>
-                    <TextField label="Username or email address" name="email" variant="outlined" />
+                    <TextField label="Username " name="email" variant="outlined" />
                     <TextField type="password" name="password" label="Password" variant="outlined" />
-                    <Button variant="contained" color="primary" type="submit">Login</Button>
+                    <span>
+                         <Button  variant="contained" color="primary" type="submit">Login</Button>
+                         <Box ml={1} component="span"><Button  type="submit" onClick={e=>setLoginComp(false)} variant="contained" color="primary" >SignUp</Button></Box>
+                    </span>
                 </form>
-            </div>
+            </div>:< div className="login-container">
+                <form className="login-box" onSubmit={e => signUp(e)}>
+                    <TextField label="Name" name="name" variant="outlined" />  
+                    <TextField label="Email" name="email" variant="outlined" />
+                    <TextField type="password" name="password" label="Password" variant="outlined" />
+                    <span>
+                         <Button  variant="contained" color="primary" type="submit">Login</Button>
+                         <Box ml={1} component="span"><Button type="submit" variant="contained" color="primary" >SignUp</Button></Box>
+                    </span>
+                </form>
+            </div>}
 
 
         </>
